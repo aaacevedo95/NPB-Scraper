b1980b3e442c4dd7885b0aae0a00337422ce62a6
aaacevedo95 <andres.a.acevedo1995@gmail.com>
2025-07-28 19:29:43 +0900

    fix: fixing github workflow

--- next.config.js
+++ next.config.js
@@ -1,16 +1,21 @@
-// Configuration options for Next.js
-const nextConfig = {
-  reactStrictMode: true, // Enable React strict mode for improved error handling
-  swcMinify: true, // Enable SWC minification for improved performance
-};
+const isProd = process.env.NODE_ENV === "production";
 
 // Configuration object tells the next-pwa plugin
 const withPWA = require("next-pwa")({
-  dest: "public", // Destination directory for the PWA files
-  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
-  register: true, // Register the PWA service worker
-  skipWaiting: true, // Skip waiting for service worker activation
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
 });
+
+// Configuration options for Next.js
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  output: "export", // Enable static export (required for GitHub Pages)
+  basePath: isProd ? "/your-repo-name" : "",
+  assetPrefix: isProd ? "/your-repo-name/" : "",
+};
 
 // Export the combined configuration for Next.js with PWA support
 module.exports = withPWA(nextConfig);
