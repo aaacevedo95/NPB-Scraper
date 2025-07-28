22f22f916628647113254ad9e1c64a8fabf8dba5
aaacevedo95 <andres.a.acevedo1995@gmail.com>
2025-07-28 19:44:43 +0900

    fix: removing github workflow, not needed

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 const isProd = process.env.NODE_ENV === "production";
 
-// Configuration object tells the next-pwa plugin
 const withPWA = require("next-pwa")({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
@@ -8,14 +7,15 @@ const withPWA = require("next-pwa")({
   skipWaiting: true,
 });
 
-// Configuration options for Next.js
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: "export", // Enable static export (required for GitHub Pages)
-  basePath: isProd ? "/your-repo-name" : "",
-  assetPrefix: isProd ? "/your-repo-name/" : "",
+  output: "export",
+  basePath: isProd ? "/NPB-Scraper" : "",
+  assetPrefix: isProd ? "/NPB-Scraper/" : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
-// Export the combined configuration for Next.js with PWA support
 module.exports = withPWA(nextConfig);
